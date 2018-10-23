523b0a6fa2827e4651c909c21cb22f3155588124
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-10-23 18:10:27 +0100

    Initial commit

---
+++ Source/AnimalAlerts/AnimalAlertsUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace AnimalAlerts
+{
+    public static class AnimalAlertsUtility
+    {
+
+        public static List<Pawn> SortedAnimalList(IEnumerable<Pawn> pawnEnumerable)
+        {
+            List<Pawn> pawnList = pawnEnumerable.ToList();
+            pawnList.SortBy(p => !p.HasBondRelation(), p => p.LabelShort);
+            return pawnList;
+        }
+
+        public static bool PlayerColonyAnimal(this Pawn p) =>
+            p.Faction == Faction.OfPlayer && p.RaceProps.Animal;
+
+        public static bool HasBondRelation(this Pawn p) =>
+            TrainableUtility.GetAllColonistBondsFor(p).Any();
+
+    }
+}
